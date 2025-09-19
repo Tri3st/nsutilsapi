d23ccef8f9e2cf3f4782fa2ebbc0a0992387d890
Martin van Diest <martin.van.diest@gmail.com>
2025-09-19 14:37:30 +0200

    Put the ALLOWED hosts in the .env file
    This way the allowed hosts local are not the same as on the server.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["www.vandiest.it","vandiest.it", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
