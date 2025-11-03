a5309f9b9b7dd5b1e1cbc77a59ab7ae0efd155fb
Martin van Diest <martin.van.diest@gmail.com>
2025-11-03 14:40:10 +0000

    And add to urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import text_to_image, LoginView, LogoutView, upload_fotos, UserInfoView
+from .views import text_to_image, LoginView, LogoutView, upload_foto, upload_fotos, UserInfoView
 
 
 print(f"LogoutView is : {LogoutView}, type: {type(LogoutView)}")
 
 urlpatterns = [
     path("text-to-image/", text_to_image, name="text_to_image"),
+    path('upload-foto/', upload_foto, name="upload_foto"),
     path('upload-fotos/', upload_fotos, name="upload_fotos"),
     path('userinfo/', UserInfoView.as_view(), name='userinfo'),
     path('login/', LoginView.as_view(), name='api-login'),
