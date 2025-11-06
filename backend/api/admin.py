f02c12adf1eeb2ac3f97f6756e430a075c55fd2e
Martin van Diest <martin.van.diest@gmail.com>
2025-11-06 14:42:09 +0000

    Add image admin bit

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from django.utils.html import format_html
+from .models import CustomUser, ExtractedImage
 
 
 # Register your models here.
@@ -13,4 +14,16 @@ class CustomUserAdmin(UserAdmin):
     )
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
+
+@admin.register(ExtractedImage)
+class ExtractedImageAdmin(admin.ModelAdmin):
+    list_display = ('original_filename', 'image_type', 'imaige_size', 'owner_username')
+    search_fields = ('original_filename', 'owner_username')
+
+    def image_preview(self, obj):
+        if obj.url:
+            return format_html('<img src="{}" width="100" />', obj.url)
+        return '-'
+    image_preview.short_description = 'Preview'
 
