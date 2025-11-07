b0186ed207ce386768ca3f45995bffea80430f69
Martin van Diest <martin.van.diest@gmail.com>
2025-11-07 13:23:51 +0000

    Add created_by to images list in admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -12,13 +12,21 @@ class CustomUserAdmin(UserAdmin):
     add_fieldset = UserAdmin.add_fieldsets + (
         (None, {'fields': ('role',)}),
     )
+    list_display = UserAdmin.list_display + ('role',)  # Voeg role toe aan de lijstweergave
 
 admin.site.register(CustomUser, CustomUserAdmin)
 
 
 @admin.register(ExtractedImage)
 class ExtractedImageAdmin(admin.ModelAdmin):
-    list_display = ('original_filename', 'image_preview', 'image_type', 'image_size', 'get_owner_username')
+    list_display = (
+        'original_filename', 
+        'image_preview', 
+        'image_type', 
+        'image_size', 
+        'get_owner_username',
+        'created_at'
+    )
     search_fields = ('original_filename', 'owner_username')
 
     def image_preview(self, obj):
